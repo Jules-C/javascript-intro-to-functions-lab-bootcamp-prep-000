7010c8de7735935f5620f43451bd4dd0671fbe06
Julie Cook <juliecook328@gmail.com>
2019-05-21 01:12:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,16 +10,10 @@ function logShout(string) {
 function logWhisper(string) {
    console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string) {
-  if (string.toUpperCase())
-  return "YES INDEED!"
-}  
+
 function sayHiToGrandma(string) {
   if (string.toLowerCase())
   return "I can't hear you!"
 }
-function sayHiToGrandma(string) {}
-  if (string === "I love you, Grandma") {
-  return "I love you, too."
-}
+
 
